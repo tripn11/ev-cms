b87572f084e1986eecefa39121e0007101c7b651
Noble <noblenwala.nn@gmail.com>
2025-06-26 23:48:44 +0100

    production setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000',
   admin: {
     user: Users.slug,
     importMap: {
