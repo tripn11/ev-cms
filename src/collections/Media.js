77cf95e54a6a5dc01544cc06419f5154358c7f75
Noble <noblenwala.nn@gmail.com>
2025-06-30 22:02:20 +0100

    url match11

--- src/collections/Media.js
+++ src/collections/Media.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-import cloudinaryAdapter from '../adapters/cloudinaryAdapter.js'; // adjust path
+import cloudinaryAdapter from '../plugins/cloudinaryAdapter.js'; // adjust path
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
